47ac5b8eb447cb5aa521ab6df87e18682f761498
fibo <casati_gianluca@yahoo.it>
2016-04-09 17:06:22 -0400

    started todos example

---
+++ todos/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//import Footer from './Footer'
+//import AddTodo from '../containers/Visible'
+
+const App = () => (
+  <div>
+    ciao
+  </div>
+)
+
+export default App
