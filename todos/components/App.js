dbf8214e6a401e32bc14d8a9d08a10279e459068
fibo <casati_gianluca@yahoo.it>
2016-04-09 19:07:42 -0400

    more on todos example

--- todos/components/App.js
+++ todos/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-//import Footer from './Footer'
-//import AddTodo from '../containers/Visible'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/Visible'
 
 const App = () => (
   <div>
-    ciao
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
   </div>
 )
 
