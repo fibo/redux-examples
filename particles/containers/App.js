c9f6e77b1e43125fdd01bb035051b9a7d75ff570
fibo <casati_gianluca@yahoo.it>
2016-04-27 14:35:55 +0200

    added some stuff

---
+++ particles/containers/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AppContainer extends Component {
+  render () {
+    return (
+      <div>particles example</div>
+    )
+  }
+}
+
+export default AppContainer
--- particles/index.js
+++ particles/index.js
@@ -5,12 +5,21 @@ import { createStore } from 'redux'
 import d3 from 'd3'
 
 import particlesApp from './reducers'
+import AppContainer from './containers/AppContainer'
+import { resizeScreen } from './actions'
 
 let store = createStore(particlesApp)
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>hello</div>
+    <AppContainer />
   </Provider>,
   document.querySelectorAll('.main')[0]
 )
+
+let onResize = function () {
+  store.dispatch(resizeScreen(window.innerWidth, window.innerHeight))
+}
+onResize()
+
+d3.select(window).on('resize', onResize)
